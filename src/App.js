3f2943130ce23c4b32a4c0b10184ea37b849597c
Stamatios Aleiferis <stamatios.aleiferis@tufts.edu>
2020-04-19 01:47:12 -0400

    init react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainLineChart from './components/MainLineChart';
+
+function App() {
+  return (
+    <div className="App">
+      <MainLineChart category="all"/>
+
+    </div>
+  );
+}
+
+export default App;
